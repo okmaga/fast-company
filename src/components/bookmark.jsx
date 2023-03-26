151915944de8cd33ce78b81d93e91f9537295b7c
Maga Bashirov <magabashirov@Magas-MacBook-Pro-2.local>
2023-03-26 15:54:21 +0300

    typo fixed

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -5,7 +5,7 @@ const BookMark = ({ status, ...rest }) => {
   return (
     <i
       className={`bi bi-bookmark${status ? "-heart-fill" : ""}`}
-      onClick={() => rest.handleToggleBookmark(rest._id)}g
+      onClick={() => rest.handleToggleBookmark(rest._id)}
     ></i>
   );
 };
