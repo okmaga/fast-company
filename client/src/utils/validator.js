946bbb86e7a8de203cc09e53112040925cf24ad9
Maga Bashirov <magabashirov@Magas-MacBook-Pro-2.local>
2023-08-20 16:49:17 +0300

    bug fix

--- client/src/utils/validator.js
+++ client/src/utils/validator.js
@@ -3,35 +3,35 @@ export function validator(data, config) {
   function validate(validateMethod, data, config) {
     let statusValidate;
     switch (validateMethod) {
-      case "isRequired": {
-        if (typeof data === "boolean") {
-          statusValidate = !data;
-        } else {
-          statusValidate = data.trim() === "";
-        }
-        break;
+    case "isRequired": {
+      if (typeof data === "boolean") {
+        statusValidate = !data;
+      } else {
+        statusValidate = data.trim() === "";
       }
-      case "isEmail": {
-        const emailRegExp = /^\S+@\S+\.\S+$/g;
-        statusValidate = !emailRegExp.test(data);
-        break;
-      }
-      case "isCapitalSymbol": {
-        const capitalRegExp = /[A-Z]+/g;
-        statusValidate = !capitalRegExp.test(data);
-        break;
-      }
-      case "isContainDigit": {
-        const digitRegExp = /\d+/g;
-        statusValidate = !digitRegExp.test(data);
-        break;
-      }
-      case "min": {
-        statusValidate = data.length < config.value;
-        break;
-      }
-      default:
-        break;
+      break;
+    }
+    case "isEmail": {
+      const emailRegExp = /^\S+@\S+\.\S+$/g;
+      statusValidate = !emailRegExp.test(data);
+      break;
+    }
+    case "isCapitalSymbol": {
+      const capitalRegExp = /[A-Z]+/g;
+      statusValidate = !capitalRegExp.test(data);
+      break;
+    }
+    case "isContainDigit": {
+      const digitRegExp = /\d+/g;
+      statusValidate = !digitRegExp.test(data);
+      break;
+    }
+    case "min": {
+      statusValidate = data.length < config.value;
+      break;
+    }
+    default:
+      break;
     };
     if (statusValidate) return config.message;
   };
